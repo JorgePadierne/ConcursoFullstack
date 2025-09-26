d6d495f41f3baafcb81ab7c642e1836e0fde020d
Jorge Padierne <jorgebolanos535@gmail.com>
2025-09-26 16:13:03 +0100

    api backend

--- ClassRoom_Dashboard_Frontend/src/App.tsx
+++ ClassRoom_Dashboard_Frontend/src/App.tsx
@@ -50,14 +50,7 @@ const App: React.FC = () => {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route
-            path="/*"
-            element={
-              <ProtectedRoute>
-                <AppRoutes />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/*" element={<AppRoutes />} />
         </Routes>
       </Router>
     </AuthProvider>
